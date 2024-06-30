116b1f0358dfe7a9babca1b2c3809f09b70620fa
MrBluesClues <lllad3nlll@gmail.com>
2024-06-30 03:48:53 -0400

    Update ModItems.java

--- src/main/java/net/Aden/yugiquest/item/ModItems.java
+++ src/main/java/net/Aden/yugiquest/item/ModItems.java
@@ -2,6 +2,7 @@ package net.Aden.yugiquest.item;
 
 import net.Aden.yugiquest.YugiQuest;
 import net.Aden.yugiquest.util.CardDataLoader;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -10,6 +11,7 @@ import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +19,7 @@ public class ModItems {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, YugiQuest.MOD_ID);
     public static final List<Map<String, Object>> CARD_DATA = CardDataLoader.loadCardData();
+    public static final List<RegistryObject<Item>> REGISTERED_CARDS = new ArrayList<>();
 
     static {
         if (CARD_DATA != null) {
@@ -28,8 +31,9 @@ public class ModItems {
 
                 LOGGER.info("Registering card: ID = " + id + ", Name = " + name + ", Image Path = " + imagePath);
 
-                final RegistryObject<Item> CARDS = ITEMS.register("card_" + id, () ->
+                final RegistryObject<Item> cardItem = ITEMS.register(String.valueOf(id), () ->
                         new Item(new Item.Properties()));
+                REGISTERED_CARDS.add(cardItem);
             }
         } else {
             LOGGER.error("Failed to load card data. CARD_DATA is null.");
@@ -38,5 +42,9 @@ public class ModItems {
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
+    }
+
+    public static List<RegistryObject<Item>> getRegisteredCards() {
+        return REGISTERED_CARDS;
     }
 }
