2a8f35a3a5347e6aff71b0ba89a3268b1b4c3e96
MrBluesClues <lllad3nlll@gmail.com>
2024-06-30 13:46:44 -0400

    Update ModItems.java
    Register PackItem

--- src/main/java/net/Aden/yugiquest/item/ModItems.java
+++ src/main/java/net/Aden/yugiquest/item/ModItems.java
@@ -2,7 +2,7 @@ package net.Aden.yugiquest.item;
 
 import net.Aden.yugiquest.YugiQuest;
 import net.Aden.yugiquest.util.CardDataLoader;
-import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -39,6 +39,18 @@ public class ModItems {
             LOGGER.error("Failed to load card data. CARD_DATA is null.");
         }
     }
+
+    // Example method to get a registered card by ID
+    public static RegistryObject<Item> getRegisteredCard(int id) {
+        return REGISTERED_CARDS.stream()
+                .filter(item -> item.getId().getPath().equals(String.valueOf(id)))
+                .findFirst()
+                .orElse(null);
+    }
+
+    // Register the common pack item (example)
+    public static final RegistryObject<Item> COMMON_PACK = ITEMS.register("common_pack", () ->
+            new PackItem(PackItem.PackType.COMMONPACK, new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
